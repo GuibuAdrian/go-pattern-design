d35e1f399fa9d3839bf89e8271a100061d59ff54
GuibuAdrian <guibuadrian@gmail.com>
2021-09-14 15:35:00 -0300

    Add multiply strategy

--- behavioral-pattern/strategy2/main.go
+++ behavioral-pattern/strategy2/main.go
@@ -10,4 +10,6 @@ func main() {
 	operation = initOperation(sub{})
 	fmt.Println(operation.op.doOperation(2,3))
 
+	operation = initOperation(multiply{})
+	fmt.Println(operation.op.doOperation(3, 9))
 }
---
+++ behavioral-pattern/strategy2/multiply.go
@@ -0,0 +1,9 @@
+package main
+
+type multiply struct {
+
+}
+
+func (op multiply) doOperation(a, b int) int {
+	return a * b
+}
