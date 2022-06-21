6b2af9110c7d683c33a942ef04ccebe6a1914e22
GuibuAdrian <guibuadrian@gmail.com>
2022-06-20 23:46:32 -0300

    Added state pattern from book

---
+++ behavioral-pattern/state2/gameContext.go
@@ -0,0 +1,12 @@
+package behavioral
+
+type GameState interface {
+	executeState(*GameContext) bool
+}
+
+type GameContext struct {
+	SecretNumber int
+	Retries      int
+	Won          bool
+	Next         GameState
+}
